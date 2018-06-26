8bd485d658a45ad026cae24a8ffe654697ce90f0
Allan Martins <allan.martins@gigigo.com.br>
2018-06-26 16:01:43 -0300

    New: Starting MarvelAPI implementation

---
+++ FarfetchSuperHeroes/FSHConstants.swift
@@ -0,0 +1,14 @@
+//
+//  FSHConstants.swift
+//  FarfetchSuperHeroes
+//
+//  Created by Allan Martins on 26/06/18.
+//  Copyright © 2018 Allan Martins. All rights reserved.
+//
+
+import Foundation
+
+struct FSHConstants {
+    static let marvelPubKey = "8edc40181745e8c28d55a0905d74d497"
+    static let marvelPvtKey = "7871e30177d23a26a9baaf065d095955d38c1388"
+}
