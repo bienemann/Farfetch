5b9933055e25c5d91123d0090b09d1a5bc3c6643
Allan Martins <allan.martins@gigigo.com.br>
2018-06-26 15:49:45 -0300

    Initial Commit

---
+++ FarfetchSuperHeroes/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FarfetchSuperHeroes
+//
+//  Created by Allan Martins on 26/06/18.
+//  Copyright © 2018 Allan Martins. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
