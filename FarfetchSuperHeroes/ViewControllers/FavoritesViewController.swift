e13cc74bee216376900a5d5032373cef1aaa27a4
Allan Martins <allan.martins@gigigo.com.br>
2018-06-27 14:37:29 -0300

    New: Start organizing view controllers

--- FarfetchSuperHeroes/ViewControllers/FavoritesViewController.swift
+++ FarfetchSuperHeroes/ViewControllers/FavoritesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  FarfetchSuperHeroes
 //
 //  Created by Allan Martins on 26/06/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FavoritesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
