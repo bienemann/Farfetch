da333d5608acb4ce81138b4f80ba728876507c6f
Allan Martins <allan.martins@gigigo.com.br>
2018-07-02 17:19:34 -0300

    New: Favorites list UI

--- FarfetchSuperHeroes/ViewControllers/FavoritesSection/FavoritesViewController.swift
+++ FarfetchSuperHeroes/ViewControllers/FavoritesSection/FavoritesViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
+    
+    @IBOutlet weak var favoritesList: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        favoritesList.register(UINib(nibName: "HeroMainCell", bundle: Bundle.main),
+                                 forCellReuseIdentifier: "hero_main_cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        favoritesList.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension FavoritesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
+    }
+    
+}
 
-
+extension FavoritesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return FavoritesManager.shared.all.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: "hero_main_cell",
+                                                     for: indexPath) as? HeroMainCell
+            else {
+                return UITableViewCell()
+        }
+        
+        cell.viewFavContainter.isHidden = true
+        cell.imgThumb.image = UIImage(named: "downloadImagePlaceholder")
+        
+        let fav = FavoritesManager.shared.all[indexPath.row]
+        cell.imgThumb.load(fav.picture!)
+        cell.lblName.text = fav.name!
+        
+        cell.selectionStyle = .none
+        return cell
+        
+    }
+    
 }
 
